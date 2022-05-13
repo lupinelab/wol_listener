2db7207430006d6b6b4d0e7bdec087d85279382c
lupinelab <jed@lupinelab.co.uk>
2022-05-13 14:04:41 +0100

    added argparse for listening port

--- wol_listener.py
+++ wol_listener.py
@@ -1,15 +1,32 @@
+#!/usr/bin/env python
+
+import argparse
 import socket
 
-def wol_listener():
-    HOST = ""
-    WAL_PORT = 9
+parser = argparse.ArgumentParser(description='Listen for Wake-on-LAN Packets')
+parser.add_argument("-p", "--port", type=int, nargs=1, help="Choose port to listen on")
+args = parser.parse_args()
+
+
+def wol_listener(port=9):
+    host = ""
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((HOST, WAL_PORT))
+        s.bind((host, port))
+        print(f"Listening for Wake-on-LAN packets on port {port}:")
         while True:
             magic_packet = s.recv(1024).hex()
-            #print(magic_packet)
             raw_mac = magic_packet.strip("f")[0:12].upper()
             mac = ':'.join(raw_mac[i:i+2] for i in range(0, len(raw_mac), 2))
             print(mac)
 
-wol_listener()
+
+if args.port:
+    try:
+        wol_listener(args.port[0])
+    except KeyboardInterrupt:
+        pass
+else:
+    try:
+        wol_listener()
+    except KeyboardInterrupt:
+        pass
